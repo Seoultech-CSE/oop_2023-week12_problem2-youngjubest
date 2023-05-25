44777f26fcdf26b4fec1878eb81c6459c71e6302
lovher <84038052+youngjubest@users.noreply.github.com>
2023-05-25 22:41:48 +0900

    한 줄에 나오게 수정

--- Problem2.java
+++ Problem2.java
@@ -15,8 +15,7 @@ public class Problem2 {
         Rational h = new Rational(-b, 2 * a);
         Rational k = new Rational(4 * a * c - b * b, 4 * a);
         
-        System.out.println("h is " + h);
-        System.out.println("k is " + k);
+        System.out.println("h is " + h + " k is " + k);
         
       
     }
